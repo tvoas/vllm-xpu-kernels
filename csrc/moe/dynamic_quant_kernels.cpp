e4bda3c797bde333913437b347a70a207de9fea4
Tanner Voas <tanner.voas@intel.com>
2026-04-29 08:43:51 +0800

    V16 - UNROLL 04 - TWG 3

--- csrc/moe/dynamic_quant_kernels.cpp
+++ csrc/moe/dynamic_quant_kernels.cpp
@@ -217,8 +217,8 @@ void moe_swiglu_dynamic_quant_impl(
                                   return launch_swiglu(unroll_tag, std::integral_constant<uint32_t, 131072>{}); // 32256 * 4 + 2048
     };
 
-    if (hidden_size %  256 == 0 && (hidden_size /  256) >= 2) return dispatch_slm(std::integral_constant<int, 4>{});
-    if (hidden_size %  128 == 0 && (hidden_size /  128) >= 2) return dispatch_slm(std::integral_constant<int, 2>{});
+    if (hidden_size %  256 == 0 && (hidden_size /  256) >= 3) return dispatch_slm(std::integral_constant<int, 4>{});
+    if (hidden_size %  128 == 0 && (hidden_size /  128) >= 3) return dispatch_slm(std::integral_constant<int, 2>{});
                                                               return dispatch_slm(std::integral_constant<int, 1>{});
 }
 
@@ -440,8 +440,8 @@ void moe_scatter_dynamic_quant_impl(
                               return launch_scatter(unroll_tag, std::integral_constant<uint32_t, 131072>{}); // 32256 * 4 + 2048
     };
 
-    if (hd_size %  256 == 0 && (hd_size /  256) >= 2) return dispatch_slm(std::integral_constant<int, 4>{});
-    if (hd_size %  128 == 0 && (hd_size /  128) >= 2) return dispatch_slm(std::integral_constant<int, 2>{});
+    if (hd_size %  256 == 0 && (hd_size /  256) >= 3) return dispatch_slm(std::integral_constant<int, 4>{});
+    if (hd_size %  128 == 0 && (hd_size /  128) >= 3) return dispatch_slm(std::integral_constant<int, 2>{});
                                                       return dispatch_slm(std::integral_constant<int, 1>{});
 }
 
