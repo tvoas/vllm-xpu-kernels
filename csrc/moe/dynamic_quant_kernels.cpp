8ab6169220f7e98015cb9a945c32539e9f23135f
Tanner Voas <tanner.voas@intel.com>
2026-05-07 17:08:04 +0800

    V22 - MUR32 - TTT256 - 21

--- csrc/moe/dynamic_quant_kernels.cpp
+++ csrc/moe/dynamic_quant_kernels.cpp
@@ -232,8 +232,8 @@ void moe_swiglu_dynamic_quant_impl(
     //int target_total_threads = 24576;
     //int target_total_threads = 32768;
     //int target_total_threads = 768;
-    int target_total_threads = 512;
-    //int target_total_threads = 256;
+    //int target_total_threads = 512;
+    int target_total_threads = 256;
     //int target_total_threads = 128;
     //int target_total_threads = 64;
 
@@ -465,8 +465,8 @@ void moe_scatter_dynamic_quant_impl(
     //int target_total_threads = 24576;
     //int target_total_threads = 32768;
     //int target_total_threads = 768;
-    int target_total_threads = 512;
-    //int target_total_threads = 256;
+    //int target_total_threads = 512;
+    int target_total_threads = 256;
     //int target_total_threads = 128;
     //int target_total_threads = 64;
 
